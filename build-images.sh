e477225e277f0508f7a7942e6b8eed01fbb4de5c
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-09-15 14:47:56 -0400

    Publish v2022.08.1. [release] (#2)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.08/Dockerfile -t cimg/gcp:2022.08.1 -t cimg/gcp:2022.08 .
+docker build --file 2022.08/Dockerfile -t cimg/azure:2022.08.1 -t cimg/azure:2022.08 .
