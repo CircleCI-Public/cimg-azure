6829a1d54859c5888e9f468627dfbff99630714d
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-27 04:39:33 -0400

    Publish v2022.10.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.09/Dockerfile -t cimg/azure:2022.09.1 -t cimg/azure:2022.09 .
+docker build --file 2022.10/Dockerfile -t cimg/azure:2022.10.1 -t cimg/azure:2022.10 .
--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/azure:2022.09
-docker push cimg/azure:2022.09.1
+docker push cimg/azure:2022.10
+docker push cimg/azure:2022.10.1
