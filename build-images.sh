0ea51a7750ddf89af5bf1dff321a535526efadbf
Adam Hartley <git@ahartley.com>
2023-04-03 09:10:54 +0100

    Publish v2023.04.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.03/Dockerfile -t cimg/azure:2023.03.1 -t cimg/azure:2023.03 .
+docker build --file 2023.04/Dockerfile -t cimg/azure:2023.04.1 -t cimg/azure:2023.04 .
