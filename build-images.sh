b0b2d7d253e02d069bfe1a8bd3481e265da4306d
Adam Hartley <BytesGuy@users.noreply.github.com>
2023-05-03 20:15:50 +0100

    Publish v2023.05.1. [release] (#11)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.04/Dockerfile -t cimg/azure:2023.04.1 -t cimg/azure:2023.04 .
+docker build --file 2023.05/Dockerfile -t cimg/azure:2023.05.1 -t cimg/azure:2023.05 .
