b45d4b8be050a14559e87fee7d08d71d3da61790
ryanbourdais <ryanbourdais@outlook.com>
2023-07-05 11:35:38 -0500

    Publish v2023.07. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.06/Dockerfile -t cimg/azure:2023.06.1 -t cimg/azure:2023.06 .
+docker build --file 2023.07/Dockerfile -t cimg/azure:2023.07 -t cimg/azure:2023.07 .
