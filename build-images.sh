701799d552f8481b2eb66dfd0b3a6b2d54d777b4
Josh Bryans <joshhsoj1902@gmail.com>
2024-03-04 17:41:37 -0500

    Update to latest cimg/deploy (#28)

--- build-images.sh
+++ build-images.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.12/Dockerfile -t cimg/azure:2023.12.1 -t cimg/azure:2023.12 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 2024.03/Dockerfile -t cimg/azure:2024.03.1 -t cimg/azure:2024.03 --push .
